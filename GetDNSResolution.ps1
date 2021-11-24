21e6635e6a461293623551e728d47307de425ed1
cnnrs22 <62676791+cnnrs22@users.noreply.github.com>
2021-11-24 17:21:28 +1030

    Add files via upload
    GetDNSResolution takes one domain per line in a txt document and returns a CSV with A and AAAA records for that domain. Outputs NXDomain.txt for those that dont resolve
    GetHTTPResponseCode takes one uri per line in a txt document and returns the response code to determine if it is still active

---
+++ GetDNSResolution.ps1
@@ -0,0 +1,33 @@
+﻿#Create I/O variables based on a list of single domains per line on the users desktop
+$DNSFilePath = "C:\Users\$env:username\Desktop\DomainNames.txt"
+$outputpath = "C:\Users\$env:username\Desktop\DNSResolution.csv"
+
+#Get the DNS Names
+$domainnames = Get-Content $DNSFilePath
+
+#Start transcript for errors (this will be where you're able to see what domains failed)
+start-transcript -path C:\Users\$env:username\Desktop\Errors.txt
+
+#Iterate over the list
+$dnsres = ForEach ($domain in $domainnames){Resolve-DnsName $domain -ErrorAction Continue}
+
+#Create Empty Array
+$DomainArray = @()
+
+#Create loop and new object to iterate and store responses
+ForEach ($d in $dnsres){
+$DNSObj = New-Object PSCustomObject -Property @{
+    Name = $d.Name
+    TTL = $d.TTL
+    Type = $d.Type
+    IP = $d.IPAddress
+    }
+    $DomainArray += $DNSObj
+}
+$DomainArray | Export-Csv $outputpath -NoTypeInformation
+
+Stop-Transcript
+
+#clean up error file and oiut
+Get-Content "C:\Users\$env:username\Desktop\Errors.txt" | Select-String "DNS name does not exist" -Context 0 | out-file "C:\Users\$env:username\Desktop\NXDomain.txt"
+Get-Content "C:\Users\$env:username\Desktop\NXDomain.txt"
---
+++ GetHTTPResponseCode.ps1
@@ -0,0 +1,15 @@
+﻿# This script will iterate over a plaintext list and return the HTTP response code
+# If the domain is not found, it will not product a HTTP response code
+# Note: This requires Powershell V7 to utilise -skiphttperrorcheck
+
+$uriFilePath = "C:\Users\$env:username\Desktop\uris.txt"
+$outputpath = "C:\Users\$env:username\Desktop\UriResponseCodes.csv"
+
+Get-Content $uriFilePath | 
+Foreach {$uri = $_; 
+try 
+    {Invoke-WebRequest -uri $uri -Method GET -Headers @{"accept"="text/html"} -useragent "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0" -skiphttperrorcheck}
+catch
+    {New-Object -TypeName psobject -Property @statuscode } } |
+ Select @{Name="RequestURI";Expression={$uri}}, StatusCode |
+ Export-Csv $outputpath
